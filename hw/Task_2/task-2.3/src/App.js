cf3f0a53938b95ef2c659eb14762140bc7aac284
Olesia Kireieva <kireieva.ol@gmail.com>
2021-08-23 18:54:22 +0300

    Task_2.3

--- hw/Task_2/task-2.3/src/App.js
+++ hw/Task_2/task-2.3/src/App.js
@@ -1,10 +1,10 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
-
-export default App;
+export default function App(props) {
+    return (
+      <button onClick={props.logThis}>
+        КНОПКА
+      </button>
+    );
+  }
