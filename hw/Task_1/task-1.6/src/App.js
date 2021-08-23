2059fd43fb8a8b334f1e5faaf392b375672ef4bc
Olesia Kireieva <kireieva.ol@gmail.com>
2021-08-23 17:26:16 +0300

    Task_1.6

--- hw/Task_1/task-1.6/src/App.js
+++ hw/Task_1/task-1.6/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import React from 'react';
+import Menu from "./menu";
+import Page from "./page";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+    }
+
+    render() {
+        console.log('fyjvv')
+        return (
+            <>
+            <Menu />
+            <Page />
+            {/* <div> <a href={"#main"}>Главная</a></div> 
+            <div> <a href={"#articles"}>Статьи</a></div> 
+            <div> <a href={"#reviews"}>Отзывы</a></div> 
+            <div> <a href={"#aboutUs"}>О нас</a></div> 
+                        
+            <div id="main">Главная</div>
+            <div id="articles">Статьи</div>
+            <div id="reviews">Отзывы</div>
+            <div id="aboutUs">О нас</div> */}
+            </>
+        )
+    }
+  
 }
 
 export default App;
