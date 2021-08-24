f88da11f1898fcbd285127611d9b332ac187a0e5
Olesia Kireieva <kireieva.ol@gmail.com>
2021-08-24 21:41:16 +0300

    Task_4.1

--- hw/Task_4/task-4.1/src/App.js
+++ hw/Task_4/task-4.1/src/App.js
@@ -1,10 +1,23 @@
+import React, {useState} from "react";
 import './App.css';
 
+const myArray = [267, 72, 445, 192, 373, 649, 792, 466, 612, 848, 372, 153, 280, 694, 263, 860, 820, 687, 567, 820,];
+// const myArray = [4, 8, 15, 16, 23, 42];
+
 function App() {
+  const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * myArray.length));
+
   return (
+    
     <div className="App">
+      <button onClick={() => setRandomNumber(Math.floor(Math.random() * myArray.length))}>
+      Выбрать случайное значение из массива
+      </button>
+      
+      <div className="randomNumber">{myArray[randomNumber]}</div>
     </div>
   );
 }
+
 
 export default App;
